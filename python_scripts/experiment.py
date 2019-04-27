01784fdc8cc89f166636d64f473a882145db3f76
BraveFalcon <averin.iv@phystech.edu>
2019-04-27 15:19:17 +0300

    Add pickle for Experiment Add cutting data

--- python_scripts/experiment.py
+++ python_scripts/experiment.py
@@ -3,17 +3,33 @@ from os import path
 import sys
 import struct
 import ctypes
+import pickle
 
 sys.path.append(path.split(path.split(path.abspath(__file__))[0])[0])
 from python_scripts import CLib
 
 
 class Experiment:
-    temperature = None
+    __dump_name = "experiment.pickle"
 
-    def __init__(self, file_path):
-        if file_path.split('.')[-1] == "bin":
-            file = open(file_path, "rb")
+    def save_pickle(self, save_path):
+        file = open(path.join(save_path, self.__dump_name), "wb")
+        pickle.dump(self, file, protocol=4)
+        file.close()
+
+    def __load_pickle(self, experiment_path):
+        file = open(path.join(experiment_path, self.__dump_name), "rb")
+        self = pickle.load(file)
+        file.close()
+
+    def __get_temperature(self):
+        return 1 / 3 * np.mean(np.sum(np.linalg.norm(self.vels, axis=2) ** 2, axis=1)) / self.num_particles
+
+    def __init__(self, experiment_path):
+        if path.exists(path.join(experiment_path, self.__dump_name)):
+            self.__load_pickle(experiment_path)
+        else:
+            file = open(experiment_path, "rb")
             self.num_frames, self.num_particles, time_per_frame, self.cell_size = struct.unpack(
                 "=iidd",
                 file.read(struct.calcsize("=iidd"))
@@ -41,17 +57,20 @@ class Experiment:
                 ctypes.POINTER(ctypes.c_double)
             ]
             bin_parser(
-                file_path.encode('ascii'),
+                experiment_path.encode('ascii'),
                 self.poses.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
                 self.vels.ctypes.data_as(ctypes.POINTER(ctypes.c_double)),
                 self.energies.ctypes.data_as(ctypes.POINTER(ctypes.c_double))
             )
 
-            self.temperature = 1 / 3 * np.mean(np.sum(np.linalg.norm(self.vels, axis=2) ** 2, axis=1)) \
-                               / self.num_particles
+            self.temperature = self.__get_temperature()
+
+            self.save_pickle(experiment_path)
 
     def cut(self, _slice):
-        pass
-
-    def save(self, savePath):
-        pass
+        self.poses = self.poses[_slice]
+        self.vels = self.vels[_slice]
+        self.energies = self.energies[_slice]
+        self.num_frames = len(self.energies)
+        self.ts = self.ts[_slice]
+        self.temperature = self.__get_temperature()
